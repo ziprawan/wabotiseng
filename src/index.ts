0e52e1adbd898e12efa9aeec0012528f75d19699
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2024-11-08 10:23:36 +0700

    bot: Read message
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,38 @@ import { BufferJSON } from "@whiskeysockets/baileys";
 import { writeFileSync } from "node:fs";
 import { Client } from "./client";
 import { mainHandler } from "./handlers";
+import { BaileysEventList } from "./types/events";
 import { Messages } from "./utils/classes/message";
 import { writeErrorToFile } from "./utils/error/write";
-import { edunexCourseListCronJob } from "./crons/edunex/course-list";
+import { FileLogger } from "./utils/logger/file";
 
-const client = new Client("opc", 10);
-const whitelist: string[] | null = process.env.WHITELISTS ? process.env.WHITELISTS.split(",") : null;
+const client = new Client(process.env.SESSION_NAME ?? "wa", 10);
+const whitelist: string[] | null = process.env.GROUPS ? process.env.GROUPS.split(",") : null;
+const unhanldedEvents: BaileysEventList[] = [
+  "blocklist.set",
+  "blocklist.update",
+  "call",
+  "chats.delete",
+  "chats.phoneNumberShare",
+  "chats.update",
+  "chats.upsert",
+  "contacts.update",
+  "contacts.upsert",
+  "group-participants.update",
+  "group.join-request",
+  "groups.update",
+  "groups.upsert",
+  "labels.association",
+  "labels.edit",
+  "message-receipt.update",
+  "messages.delete",
+  "messages.media-update",
+  "messages.reaction",
+  "messages.update",
+  "messaging-history.set",
+];
 
-client.addEventHandler("messages.upsert", async (_sock, event) => {
+client.addEventHandler("messages.upsert", async (sock, event) => {
   const { messages } = event;
 
   event.messages.forEach(async (msgEv) => {
@@ -25,12 +49,27 @@ client.addEventHandler("messages.upsert", async (_sock, event) => {
     }
   });
 
+  const keys = event.messages.map((m) => m.key);
+
+  await sock.readMessages(keys);
+
   messages
     .filter((m) => m.key.remoteJid === process.env.DEBUG_ID)
     .forEach((m) => {
       writeFileSync(`json/messages/upsert/${Date.now()}.json`, JSON.stringify(m, BufferJSON.replacer, 2));
     });
 });
+
+if (process.env.IS_DEBUG === "true") {
+  unhanldedEvents.forEach((ev) => {
+    client.addEventHandler(ev, async (sock, event) => {
+      const logger = new FileLogger(`${ev}-${Date.now()}`);
+      logger.write(`Event: ${ev}`);
+      logger.write(`Data: ${JSON.stringify(event, BufferJSON.replacer, 2)}`);
+      logger.close();
+    });
+  });
+}
 
 // client.addCron("edunex", "* */10 * * * *", edunexCourseListCronJob);
 
