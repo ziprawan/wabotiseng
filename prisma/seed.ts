6efa16a1da3a88154d650c39193ffd2bedb6e1d9
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2024-11-13 17:59:37 +0700

    db: Add field deleted for Messages model
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import { proto } from "@whiskeysockets/baileys";
+
+const botDatabase = new PrismaClient();
+
+async function main() {
+  await botDatabase.message.updateMany({ data: { deleted: false } });
+  const deletes = await botDatabase.message.findMany({
+    where: { message: { contains: `"type":"REVOKE"` }, credsName: process.env.SESSION_NAME ?? "wa" },
+  });
+
+  for (let i = 0; i < deletes.length; i++) {
+    const msg = deletes[i];
+    const raw = JSON.parse(msg.message) as proto.WebMessageInfo;
+    const ptc = raw.message?.protocolMessage;
+
+    if (!ptc) {
+      console.log("Skipping...");
+      continue;
+    }
+
+    console.log(msg.remoteJid, ptc.key?.id);
+
+    if (msg.remoteJid === "status@broadcast") {
+      await botDatabase.message.delete({
+        where: {
+          messageId_remoteJid_credsName: {
+            credsName: process.env.SESSION_NAME ?? "wa",
+            messageId: ptc.key?.id ?? "",
+            remoteJid: msg.remoteJid,
+          },
+        },
+      });
+    } else {
+      await botDatabase.message.update({
+        where: {
+          messageId_remoteJid_credsName: {
+            credsName: process.env.SESSION_NAME ?? "wa",
+            messageId: ptc.key?.id ?? "",
+            remoteJid: msg.remoteJid,
+          },
+        },
+        data: { deleted: true },
+      });
+    }
+  }
+
+  await botDatabase.message.deleteMany({
+    where: { message: { contains: '"type":"REVOKE"' }, credsName: process.env.SESSION_NAME ?? "wa" },
+  });
+}
+
+main()
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("DONE!!!");
+  });
