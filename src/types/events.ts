f7b983f1e8d722e01cdb94d058927a5ef044762e
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2024-12-13 14:36:53 +0700

    types: Change *HandlerFunc return type to Promise<any>
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,9 +1,10 @@
 import makeWASocket, { BaileysEventMap, BinaryNode } from "@whiskeysockets/baileys";
 import { WASocket } from "./socket";
 
+export type MaybePromise<T> = Promise<T> | T;
 export type BaileysEventList = keyof BaileysEventMap;
 export type EventHandlerFunc<T extends BaileysEventList> = (
   client: ReturnType<typeof makeWASocket>,
   arg: BaileysEventMap[T]
-) => Promise<void>;
-export type WSHandlerFunc = (client: WASocket, node: BinaryNode) => Promise<void>;
+) => Promise<any>;
+export type WSHandlerFunc = (client: WASocket, node: BinaryNode) => Promise<any>;
