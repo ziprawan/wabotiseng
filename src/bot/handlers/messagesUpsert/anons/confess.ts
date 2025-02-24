1922c145360e0ac81562c2f67befa4862764cda0
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-02-24 08:31:15 +0700

    confess: Try to add media message support
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- src/bot/handlers/messagesUpsert/anons/confess.ts
+++ src/bot/handlers/messagesUpsert/anons/confess.ts
@@ -1,5 +1,6 @@
 import { projectConfig } from "@/config";
 import { CommandHandlerFunc } from "#bot/types/command/handler";
+import { AnyMessageContent, downloadMediaMessage, WAMessage } from "@whiskeysockets/baileys";
 
 const CONFESS_TARGET = projectConfig.CONFESS_TARGET;
 
@@ -10,13 +11,63 @@ export const confessHandler: CommandHandlerFunc = async ({ msg, parser, sock }) 
     return;
   }
 
-  const cmd = parser.command() as string;
-  const pfx = parser.usedPrefix() as string;
-  const confessMsg = parser.text.replace(pfx + cmd, "").trim();
+  const confessMsg = msg.reply_to_message ? msg.reply_to_message : msg;
 
-  if (confessMsg === "") {
-    return await msg.replyText("Kasih pesannya dong kak (⁠ ⁠･ั⁠﹏⁠･ั⁠)");
+  async function resolveMedia(rawMsg: WAMessage): Promise<Buffer | null> {
+    try {
+      return await downloadMediaMessage(rawMsg, "buffer", {});
+    } catch {
+      await msg.replyText("Terjadi kesalahan saat mengunduh media.");
+      return null;
+    }
   }
 
-  return await sock.sendMessage(CONFESS_TARGET, { text: `Chat! Someone confessed that\n${confessMsg}` });
+  const raw = confessMsg.raw;
+  const rawMsg = raw.message;
+  const txt = confessMsg.text;
+  let caption = "";
+
+  if (!rawMsg) {
+    return await msg.replyText("Ada yang salah saat mengambil konten pesan!");
+  }
+
+  if (txt) {
+    const args = parser.args();
+    const currentText = args[1];
+
+    if (currentText) {
+      caption = `Chat! Ada konfes dari seseorang nih!\n`;
+      if (txt) caption += txt + "\n\n";
+      caption += parser.text.slice(currentText.start);
+    }
+  }
+
+  let sendMessageContent: AnyMessageContent | null = null;
+  const media = await resolveMedia(raw);
+
+  if (rawMsg["audioMessage"]) {
+    sendMessageContent = media && { audio: media, ptt: false, caption };
+  } else if (rawMsg["videoMessage"]) {
+    sendMessageContent = media && { video: media, caption };
+  } else if (rawMsg["imageMessage"]) {
+    sendMessageContent = media && { image: media, caption };
+  } else if (rawMsg["documentMessage"]) {
+    sendMessageContent = media && { document: media, mimetype: rawMsg.documentMessage.mimetype ?? "text/plain", caption };
+  } else if (rawMsg["stickerMessage"]) {
+    sendMessageContent = media && { sticker: media };
+  } else if (rawMsg["ptvMessage"]) {
+    sendMessageContent = media && { video: media, ptv: true, caption };
+  } else if (txt !== "" && caption !== "") {
+    sendMessageContent = { text: caption };
+  }
+
+  try {
+    if (!sendMessageContent) {
+      return await msg.replyText("Kasih pesannya dong kak", true);
+    }
+
+    return await sock.sendMessage(projectConfig.CONFESS_TARGET, sendMessageContent);
+  } catch {
+    return await msg.replyText("Terjadi kesalahan saat mengirim pesan");
+  }
 };
